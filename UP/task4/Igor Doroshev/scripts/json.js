386d5a8afaf1bda9a0dc48f313ebdab44a89a8c6
idoroshev <igdor9399@gmail.com>
2017-03-22 00:47:19 +0300

    Igor Doroshev completed Task4

---
+++ UP/task4/Igor Doroshev/scripts/json.js
@@ -0,0 +1,73 @@
+window.poc = {};
+window.poc.jsonData='[\
+{\
+	"id": "1",\
+	"imageUrl": "Images/news1.jpg",\
+	"info": "«Венгер — уходи» vs «Мы верим в Арсена»",\
+	"description": "Баннерная война болельщиков «Арсенала».",\
+	"link": "https://www.sports.ru/tribuna/blogs/fotoblog/1215951.html"\
+},\
+\
+{\
+	"id": "2",\
+	"imageUrl": "Images/news2.jpg",\
+	"info": "Чего «Ростову» не хватило в Манчестере",\
+	"description": "Команда Бердыева покидает еврокубки, но за нее точно не стыдно.",\
+	"link": "https://www.sports.ru/tribuna/blogs/fczenitspb/1214624.html"\
+},\
+\
+{\
+	"id": "3",\
+	"imageUrl": "Images/news3.jpg",\
+	"info": "Моуринью передает Рохо банан",\
+	"description": "Что это было?",\
+	"link": "https://www.sports.ru/tribuna/blogs/odukhevremeni/1214496.html"\
+},\
+\
+{\
+	"id": "4",\
+	"imageUrl": "Images/news4.jpg",\
+	"info": "«Барселона», ты просто космос",\
+	"description": "Величайший день в истории футбола.",\
+	"link": "https://www.sports.ru/tribuna/blogs/fotoblog/1206802.html"\
+},\
+\
+{\
+	"id": "5",\
+	"imageUrl": "Images/news5.jpg",\
+	"info": "Луис Энрике видел будущее?",\
+	"description": "Тренер «Барселоны» ответил за слова.",\
+	"link": "https://www.sports.ru/tribuna/blogs/odukhevremeni/1206852.html"\
+},\
+\
+{\
+	"id": "6",\
+	"imageUrl": "Images/news6.jpg",\
+	"info": "Лучшие фото, где Роналду и Месси вместе",\
+	"description": "Две легенды на одном снимке.",\
+	"link": "https://www.sports.ru/tribuna/blogs/fotoblog/1175886.html"\
+},\
+\
+{\
+	"id": "7",\
+	"imageUrl": "Images/news7.jpg",\
+	"info": "Крутые бомбардиры из клубов-середняков",\
+	"description": "Ди Натале, Тони и другие люди, которые нас удивляли.",\
+	"link": "https://www.sports.ru/tribuna/blogs/codfo18/1199497.html"\
+},\
+\
+{\
+	"id": "8",\
+	"imageUrl": "Images/news8.jpg",\
+	"info": "Настоящий «Лестер» вернулся",\
+	"description": "Это снова та команда, за которую болел весь мир.",\
+	"link": "https://www.sports.ru/tribuna/blogs/odukhevremeni/1212472.html"\
+},\
+\
+{\
+	"id": "9",\
+	"imageUrl": "Images/news9.jpg",\
+	"info": "15 лет самому красивому голу в истории АПЛ",\
+	"description": "В начале весны 2002-го Деннис Бергкамп сотворил это.",\
+	"link": "https://www.sports.ru/tribuna/blogs/england/747098.html"\
+}]';
