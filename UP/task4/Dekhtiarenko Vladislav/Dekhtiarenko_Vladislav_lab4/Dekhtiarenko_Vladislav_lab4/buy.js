e570b3761acab07a5df932a3475f0449587db5a3
VladDekht <vladislav.dekhtiarenko@yandex.ru>
2017-03-25 19:20:00 +0300

    Vladislav Dekhtiarenko completed task 4

---
+++ UP/task4/Dekhtiarenko Vladislav/Dekhtiarenko_Vladislav_lab4/Dekhtiarenko_Vladislav_lab4/buy.js
@@ -0,0 +1,43 @@
+﻿document.addEventListener("DOMContentLoaded", page);
+
+function page() {
+    var data = JSON.parse(window.data.cars);
+    var wrapper = document.getElementById("wrapper");
+    
+    for (var i = 0; i < 5; i++) {
+        var div = document.createElement("div");
+        div.className = ("block");
+        div.id = data[i].id;
+
+        var image = document.createElement("img");
+        image.src = data[i].imageUrl;
+        image.className = ("images");
+
+        var h1 = document.createElement("h1");
+        h1.innerHTML = data[i].name;
+
+        var p = document.createElement("p");
+        p.innerHTML = data[i].description;
+
+        div.appendChild(h1);
+        div.appendChild(p);
+        div.appendChild(image);
+        wrapper.appendChild(div);
+    }
+}
+
+function search() {
+    var input = document.getElementById("myInput");
+    var filter = input.value.toUpperCase();
+    for (j = 0; j < 9; j++) {
+        var searchDiv = document.getElementById("id" + j);
+        if (searchDiv) {
+            if ((searchDiv.childNodes[1].innerHTML.toUpperCase().indexOf(filter) > -1) || (searchDiv.childNodes[0].innerHTML.toUpperCase().indexOf(filter) > -1)) {
+                searchDiv.style.display = "";
+            } else {
+                searchDiv.style.display = "none";
+            }
+        }
+    }
+
+}
