db537cc710f093881c5a1a9fe68a400f9e93d013
Dmitry <digger0007@mail.ru>
2017-04-09 21:18:26 +0300

    task 4 finished

---
+++ UP/task4/Kurch_Dmitry/information.js
@@ -0,0 +1,73 @@
+window.information={};
+window.information.cakes='[\
+  {\
+    "id": "1",\
+    "picture": "pictures/shortened_cake.jpg",\
+    "cake": "Shortened Cake",\
+    "description": "Shortened cakes include cakes made with all kinds of fats, including butter,\
+     lard, vegetable shortening, and more. But this category is often referred to as *butter cakes* \
+     as a catch-all term because of butter*s proliferance in baking recipes.\
+     They are considered "shortened" because the high amount of butter or fat actually \
+     shortens the gluten strands that form when flour comes in contact with water. \
+     The shorter gluten strands result in a more tender texture, which creates a delicate, soft crumb."\
+  },\
+  {\
+    "id": "2",\
+    "picture": "pictures/pound_cake.jpg",\
+    "cake": "Pound Cake",\
+    "description": "Pound cake is just about the butteriest of butter cakes out there. \
+    The name "pound cake" comes from the French "quatre quarts," which refers to the equal amount \
+    of the key ingredients (flour, butter, sugar and eggs) in the cake.\
+    Compared to other butter cakes, the ratio of butter or fat is much higher, which gives \
+    the resulting cake a dense texture, tight crumb and sturdy shape. Pound cakes work extremely \
+    well for cake decorating projects, as they will hold their shape even when sculpted, and they \
+    won*t collapse under the weight of fondant or buttercream. "\
+  },\
+  {\
+    "id": "3", \
+    "picture": "pictures/foam_cake.jpg",\
+    "cake": "Foam and Sponge Cakes",\
+    "description": "Nestled under this category are cakes such as angel food cake, genoise, dacquoise or jelly\
+     roll-style cakes. Eggs are the name of the game with fluffy foam or sponge cakes.\
+     Usually, air is beaten into the eggs (sometimes just the whites, sometimes the whole egg),\
+     and then they are folded into the batter. During the baking process, this creates steam in \
+     the batter, causing the cakes to rise."\
+  }\
+  {\
+    "id": "4", \
+    "picture": "pictures/egg_cake.jpg",\
+    "cake": "Egg Whites Only",\
+    "description": "Cakes made with egg whites only are light, airy and naturally low in fat.\
+    Perhaps the most famous example of this type of cake is angel food cake.\
+    The whipped egg whites assist in attaining a spectacular rise, so specific pans are required\
+    for cakes made solely with egg whites. Think of the famous angel food cake pan, with its\
+    high sides and circular cutout in the center, which allows for even baking and rising. "\
+  }\
+  {\
+    "id": "5", \
+    "picture": "pictures/flourless_cake.jpg",\
+    "cake": "Flourless Chocolate Cake",\
+    "description": "A great example is flourless chocolate cake, in which whipped egg whites are folded\
+    into a rich mixture of chocolate, egg yolk and butter or cream, and egg yolk. While the method is similar\
+    to a sponge cake, its ingredient list makes it decidedly different from a roulade or sponge cake.\
+    Some cakes are "almost-flourless" — chocolate cakes contain a very small amount of flour\
+    (from a few tablespoons to 1/4 cup or so), but would still fall under this category. "\
+  }\
+  {\
+    "id": "6", \
+    "picture": "pictures/unbaked_cake.jpg",\
+    "cake": "Unbacked Cakes",\
+    "description": "Unbaked cakes are confections that require no baking at all. \
+    This category would include desserts such as no-bake cheesecake, ice cream cakes and icebox cakes.\
+    It*s more the construction and presentation than the ingredients which classify these desserts as *cakes.* "\
+  }\
+  {\
+    "id": "7", \
+    "picture": "pictures/yeast_cake.jpg",\
+    "cake": "Yeast Cakes",\
+    "description": "While most people think of yeast as a bread ingredient, it*s also employed in many cakes.\
+    Cakes made with yeast tend to have a much sturdier texture and a more mild sweetness. With a less tender\
+    crumb, they aren*t likely to be used for birthday or wedding cakes, but instead for more hearty cakes\
+    such as coffee cakes or the famous New Orleans specialty, King Cake."\
+  }\
+]';
