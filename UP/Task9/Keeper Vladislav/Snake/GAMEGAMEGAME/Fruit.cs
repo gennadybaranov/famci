b9ccc8c911aad94b2f0305d5da947a4330b1ef62
Gennady Baranov <gennadybaranov@gmail.com>
2017-06-04 22:02:24 +0300

    Snake completed

---
+++ UP/Task9/Keeper Vladislav/Snake/GAMEGAMEGAME/Fruit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAMEGAMEGAME
+{
+    public class Fruit:IDraw
+    {
+        public int _x;
+        public int _y;
+        public Fruit(int x,int y)
+        {
+            _x = x;
+            _y = y;
+        }
+
+        public void Draw(Graphics gr, int a)
+        {
+            gr.FillRectangle(new SolidBrush(Color.Red),
+                new Rectangle(_x * a, _y * a, a, a));
+        }
+    }
+}
