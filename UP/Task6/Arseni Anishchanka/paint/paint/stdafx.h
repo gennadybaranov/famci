40bd527910b180525eaa30fe64cf202b90370e1f
Aniščanka Arssenij <BatsilaBox3@gmail.com>
2017-06-06 21:16:28 +0300

    Arseni Anishchanka complete task 6

---
+++ UP/Task6/Arseni Anishchanka/paint/paint/stdafx.h
@@ -0,0 +1,31 @@
+// stdafx.h: включаемый файл для стандартных системных включаемых файлов
+// или включаемых файлов для конкретного проекта, которые часто используются, но
+// не часто изменяются
+//
+
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include "targetver.h"
+
+#define WIN32_LEAN_AND_MEAN             // Исключите редко используемые компоненты из заголовков Windows
+// Файлы заголовков Windows:
+#include <windows.h>
+
+// Файлы заголовков C RunTime
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+#include <windows.h>
+#include <windowsx.h>
+#include <cstring>
+#include <string>
+#include <stdio.h>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <commdlg.h>
+#include <fstream>
+
+
+// TODO: Установите здесь ссылки на дополнительные заголовки, требующиеся для программы
