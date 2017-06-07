d6ce85435f52e5c7a8c80b7c8bdb2d26e7372cb8
Gennady Baranov <gennadybaranov@gmail.com>
2017-06-07 20:39:33 +0300

    першая версія Стэка рэалізаваныя пытанні, іх узаемасувязь з карыстальнікамі існуе магчымасць стварыць адказ на пытанне, аднак пасля гэтага з'яўляецца памылка

---
+++ UP/Task10/StackOverflow/ver 1.0/stack_solution/STACK/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STACK.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual ICollection<Answer> Answers { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public virtual ApplicationUser ApplicationUser { get; set; }
+    }
+}
